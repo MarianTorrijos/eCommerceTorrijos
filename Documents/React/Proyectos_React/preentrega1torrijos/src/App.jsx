dd490d3ec60082077121902264d737c4a67add73
marian <marianag@hotmail.com>
2024-07-11 20:40:39 -0300

    primer commit

---
+++ Documents/React/Proyectos_React/preentrega1torrijos/src/App.jsx
@@ -0,0 +1,25 @@
+import { NavBar } from "./components/NavBar";
+import { ItemListContainer } from "./components/ItemListContainer";
+
+
+
+
+
+
+function App() {
+  return (
+  <>
+  <NavBar />
+  <ItemListContainer greeting="Muchas gracias"/>
+ 
+ 
+
+  
+
+  </>
+  );
+}
+
+
+
+export default App;
---
+++ Documents/React/Proyectos_React/preentrega1torrijos/src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import { CartWidget } from "./CartWidget";
+
+export const NavBar = () => <>
+<h6>Seleccione una opcion</h6>
+<ul>
+    <li><a href="#">Alimentos</a></li>
+    <li><a href="#">Bebidas</a></li>
+    <li><a href="#">Snacks</a></li>
+    
+    </ul>
+    <CartWidget/>
+</>;
